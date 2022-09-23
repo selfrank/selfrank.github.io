bd6d9ce8566b2e80cde757c437741b8e270af7fb
Sarah Ibarra-Scurr <sarah.elfrank@gmail.com>
2022-09-22 19:09:17 -0500

    removing extra files and renaming to docs

--- Profile_project/app.js
+++
@@ -1,9 +0,0 @@
-new hoverEffect({
-    parent: document.querySelector('.img'),
-    image1: './footer_bg_08.png',
-    image2: './footer_bg_09.png',
-    displacementImage:'./displacementImage2.jpg'
-})  
-
-const dropdownElementList = document.querySelectorAll('.dropdown-toggle')
-const dropdownList = [...dropdownElementList].map(dropdownToggleEl => new bootstrap.Dropdown(dropdownToggleEl))
